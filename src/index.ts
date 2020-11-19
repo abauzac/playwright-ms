6fcac3a1a49ed076f0c69bd5a840ba7ad526a4ac
Iurii <y.tikhenkiy@gmail.com>
2020-11-19 16:30:53 +0200

    goto auth + making screenshot

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,44 @@
 import * as playwright from 'playwright';
 
 async function main() {
-    // Launch browser
-    const browser = await playwright.chromium.launch();
-    // Create context
-    const context = await browser.newContext(); 
-    // Open new page
-    const page = await context.newPage();
+    const{chromium, webkit, firefox} = playwright;
+    for(const browserType of [chromium, webkit, firefox]){
 
-    await page.goto('https://paperhelp.org');
-    await page.screenshot({
-        path: `screenshot/ ${playwright.chromium.name()}.png`
-    });
+        // Launch browser
+        const browser = await browserType.launch({
+            headless: true
+        });
+        // Create context
+        const context = await browser.newContext(); 
+
+        // Open new page
+        const page = await context.newPage();
+        // await page.setViewportSize({
+        //     width: 1440,
+        //     height:900,
+        // })
+
+        await page.goto('http://paperhelp.org');
+
+        // await page.click('[data-ph-tst="hdr-ordr_now"]');
+        await page.click('css=[data-ph-tst=hdr-sgn_in]');
+        
+        await page.waitForTimeout(5000);
+
+        let today = new Date();
+        let hh = String(today.getHours());
+        let mm = String(today.getMinutes()); 
+        let sec = String(today.getSeconds()); 
+        let currentTime = hh + ':' + mm + ':' + sec;
+
+        await page.screenshot({
+            path: `screenshot/ ${browserType.name()} ${currentTime}.png`
+        });
+
+        // Close the browser
+        await browser.close();
+    }
     
-    // Close th browser
-    await browser.close();    
+    
 }
 main();
